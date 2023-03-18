5b7634307f5cfa3897d706abc0e00511be443e99
kj7722 <labit.kj1@gmail.com>
2023-03-18 21:23:41 +0900

    test_1

---
+++ src/main/java/org/zerock/controller/RootConfig.java
@@ -0,0 +1,10 @@
+package org.zerock.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"org.zerock.sample"})
+public class RootConfig {
+
+}
---
+++ src/main/java/org/zerock/controller/WebConfig.java
@@ -0,0 +1,23 @@
+package org.zerock.controller;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends
+	AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+		return null;
+	}
+
+}
